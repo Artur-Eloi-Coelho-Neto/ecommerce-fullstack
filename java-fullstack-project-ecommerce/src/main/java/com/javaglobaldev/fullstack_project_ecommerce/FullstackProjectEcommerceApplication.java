8758e0adb1e6a70b93687d767e2ff6f1329f3922
Artur-Eloi-Coelho-Neto <artureloiwork@gmail.com>
2026-09-06 18:42:01 -0300

    java-project-ecommerce

---
+++ java-fullstack-project-ecommerce/src/main/java/com/javaglobaldev/fullstack_project_ecommerce/FullstackProjectEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.javaglobaldev.fullstack_project_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjectEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackProjectEcommerceApplication.class, args);
+	}
+
+}
---
+++ java-fullstack-project-ecommerce/src/test/java/com/javaglobaldev/fullstack_project_ecommerce/FullstackProjectEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaglobaldev.fullstack_project_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
